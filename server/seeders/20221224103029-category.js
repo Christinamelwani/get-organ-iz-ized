e67b572c4b829218fa7d4d6e78f68e362108e637
Christinamelwani <christinarajm@gmail.com>
2022-12-24 17:43:31 +0700

    Add categories seeder

---
+++ server/seeders/20221224103029-category.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+
+const categories = require("../data/categories.json");
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    categories.forEach((category) => {
+      category.createdAt = new Date();
+      category.updatedAt = new Date();
+    });
+
+    await queryInterface.bulkInsert("Categories", categories);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Categories", null, {});
+  },
+};
