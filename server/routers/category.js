4d2e0557a5c16d60615ae773da35c6a084df6bbb
Christinamelwani <christinarajm@gmail.com>
2022-12-24 18:23:54 +0700

    Implement fetch categories

--- server/routers/category.js
+++ server/routers/category.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const categoryRouter = express.Router();
+const CategoryController = require("../controllers/categoryController");
 
-categoryRouter.get("/");
+categoryRouter.get("/", CategoryController.getCategories);
 
 module.exports = categoryRouter;
